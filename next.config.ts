54ba2e646256eefef5cd39fbd9aafd9acc54a0e6
Daniel Gao <daniel.gao6205@gmail.com>
2026-01-12 20:12:02 -0500

    removed static stuff

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Needed for `next export`
-  trailingSlash: true, // Optional: ensures / at end of URLs for Firebase
-  images: {
-    unoptimized: true, // Required for Firebase static hosting (no Next.js Image optimization)
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
